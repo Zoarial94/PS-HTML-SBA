028bd885b4761b40132123a8aff59223249d8ce4
Hunter White <HWB94@protonmail.com>
2021-05-19 19:52:37 -0400

    Updated content, styles, and JS.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 console.log("My Script!")
 
-var reportButton = document.getElementById("report-problems-button")
 
+/* Higher order function nonsense. Learned from a React tutorial.
+ * On second glance, it's not quite the same thing, but same idea. 
+**/
 var redirect = (link) => { 
     window.location.href = link;
 }
-
 var redirectToProblemReport = () => {
     redirect("https://youtu.be/dQw4w9WgXcQ");
 }
 
+// Make button redirect to link
+var reportButton = document.getElementById("report-problems-button")
 reportButton.addEventListener("click", redirectToProblemReport);
--- special.js
+++ special.js
@@ -1,4 +1,5 @@
 /* CORS prevents this stuff
+
 var info = document.getElementById("dynamicInformation")
 
 const url='https://xkcd.com/info.0.json';
@@ -16,18 +17,30 @@ fetch(url).then(data => {
 let form = document.getElementById("form")
 let output = document.getElementById("formOutput")
 
+// Form validation
 form.addEventListener("submit", (event) => {
+    // Prevent the form from actually submitting
     event.preventDefault();
     let email = form.elements["email"].value;
+    let name = form.elements["name"].value;
+
+    // emailregex.com "Email Address Regular Expression That 99.99% Works."
     let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if(!emailRegex.test(email)) {
+    // Test for an email
+    if (!emailRegex.test(email)) {
         console.log("Invalid email!");
         output.textContent = "Invalid email: \"" + email + "\". Try again.";
-        return false;
+        output.style.color = "red";
+    // Test for a name
+    } else if (name === "") {
+        output.textContent = "Please enter a name."
+        output.style.color = "red";
+    // Vaild form. Continue
     } else {
-        console.log("Valid email!");
-        output.textContent = "Submitted!";
+        output.textContent = "Thank you " + name + "! You have been signed up!";
+        output.style.color = "green";
     }
+    // Prevent the form from actually submitting (I think)
     return false;
 })
